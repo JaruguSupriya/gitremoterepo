01bc2149562f752de839bc88459d603617b004d7
JARUGU SUPRIYA <142426723+JaruguSupriya@users.noreply.github.com>
2025-01-09 21:10:01 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,11 @@
+
+m = int(input("Enter the number of elements in the list: "))
+numbers = []
+for i in range(m):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+even_numbers = [num for num in numbers if num % 2 == 0]
+sum_of_even_numbers = sum(even_numbers)
+print("\nOriginal List:", numbers)
+print("Even Numbers:", even_numbers)
+print("Sum of Even Numbers:", sum_of_even_numbers)
